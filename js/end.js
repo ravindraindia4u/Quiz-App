6a0a56f5a4339e98940fee8a6f23c9493f6b3d54
ravindraindia4u <ravindraindia4u@gmail.com>
2020-05-29 03:19:32 +0530

    bug fix regarding goto home page

--- js/end.js
+++ js/end.js
@@ -22,5 +22,5 @@ saveHighScore = e =>{
     highScores.sort((a,b)=>b.score - a.score);
     highScores.splice(5);
     localStorage.setItem("highScores",JSON.stringify(highScores));
-    window.location.assign("../index.html");
+    window.location.assign("../");
 };
