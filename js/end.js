9aff3e694aa39b10487136f28265babcf6187047
ravindraindia4u <ravindraindia4u@gmail.com>
2020-05-29 12:23:53 +0530

    bug fix

--- js/end.js
+++ js/end.js
@@ -22,5 +22,5 @@ saveHighScore = e =>{
     highScores.sort((a,b)=>b.score - a.score);
     highScores.splice(5);
     localStorage.setItem("highScores",JSON.stringify(highScores));
-    window.location.assign("/");
+    window.location.assign("../");
 };
